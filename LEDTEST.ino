4209e7961a2d5f8eb99668c52e249d4e25d1558e
cathyfinn744 <s00291747@atu.ie>
2026-02-09 14:31:59 +0000

    Create LEDTEST.ino

---
+++ LEDTEST.ino
@@ -0,0 +1,17 @@
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int knobValue = analogRead(A0);
+  if (knobValue > 400){
+    digitalWrite(2, HIGH);
+  }else { digitalWrite(2, LOW);}
+   // print out the value you read:
+  Serial.println(knobValue);
+
+}
